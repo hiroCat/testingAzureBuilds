9466e42d7fc7f1eb4c3c7178e779ec823f6ae270
Miquel Puigdomenech <miki2189@gmail.com>
2019-10-26 21:23:39 +0200

    dsdfasdfasfdsdfasdfasdfasdf

--- src/projectA/ConsoleAppA/Program.cs
+++ src/projectA/ConsoleAppA/Program.cs
@@ -8,11 +8,6 @@ namespace ConsoleAppA
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the number of nugets");
-
-            var n = Console.Read();
-            var fN = Frier.DoFry(n);
-            foreach (var item in fN)
-                Console.WriteLine(item);
         }
     }
 }
